53b2518437cc043ce2b98779d4888b657f5f9987
unional <homawong@gmail.com>
2024-01-21 13:03:56 -0800

    chore: use @repobuddy/biome

--- ts/get_language_tool_version.ts
+++ ts/get_language_tool_version.ts
@@ -2,7 +2,9 @@ import assert from 'node:assert'
 import { type Pom } from './read_pom.js'
 
 export function getLanguageToolVersion(pom: Pom) {
-	const langTool = pom.project.dependencies.dependency.find((d) => d.groupId === 'org.languagetool' && d.artifactId === 'languagetool-core')
+	const langTool = pom.project.dependencies.dependency.find(
+		(d) => d.groupId === 'org.languagetool' && d.artifactId === 'languagetool-core'
+	)
 	assert(langTool, 'Unable to find org.languagetool > languagetool-core')
 	return langTool.version
 }
