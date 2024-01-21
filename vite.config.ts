f55a5e0b51d7b1127d812e758486fed712296fb1
unional <homawong@gmail.com>
2024-01-21 01:52:30 -0800

    chore: add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
+		coverage: {
+			exclude: ['cjs', 'esm']
+		},
 		include: ['ts/**/*.{spec,test,unit,accept,integrate,system,perf,stress}.ts']
 	}
 })
