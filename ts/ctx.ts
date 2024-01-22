7d0648f41f00dceed8d6d53f18a8f7d570f63a9e
unional <homawong@gmail.com>
2024-01-21 16:41:01 -0800

    chore: add verify script

--- ts/ctx.ts
+++ ts/ctx.ts
@@ -2,7 +2,7 @@ import { getLtexLsLatestRelease } from './get_ltex_ls_latest_release.js'
 import { readPom, type Pom } from './read_pom.js'
 
 export const _store: {
-	ltexLsRelease?: Awaited<ReturnType<typeof getLtexLsLatestRelease>>,
+	ltexLsRelease?: Awaited<ReturnType<typeof getLtexLsLatestRelease>>
 	pom?: Pom
 } = {}
 
--- ts/get_ltex_ls_download_url.spec.ts
+++ ts/get_ltex_ls_download_url.spec.ts
@@ -1,6 +1,5 @@
 import { a } from 'assertron'
 import { it } from 'vitest'
-import { ctx } from './ctx.js'
 import { getLtexLsDownloadUrl } from './get_ltex_ls_download_url.js'
 import { getLtexLsVersion } from './get_ltex_ls_version.js'
 
@@ -9,7 +8,7 @@ it('gets url by platform', async () => {
 	const l = await getLtexLsDownloadUrl('linux')
 	const m = await getLtexLsDownloadUrl('mac')
 
-	const version = getLtexLsVersion( ctx.readPom())
+	const version = getLtexLsVersion()
 
 	a.satisfies(w, new RegExp(`ltex-ls\/releases\/download\/16.0.0\/ltex-ls-${version}-windows-x64.zip`))
 	a.satisfies(l, new RegExp(`ltex-ls\/releases\/download\/16.0.0\/ltex-ls-${version}-linux-x64.tar.gz`))
